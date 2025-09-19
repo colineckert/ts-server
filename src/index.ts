02a885256115c6e0042cfb7fe3d2a4db155b076b
Colin Eckert <colineckert10@gmail.com>
2025-09-19 06:08:13 -0700

    add readiness handler

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,15 @@
-import express from "express";
+import express, { Request, Response } from 'express';
 
 const app = express();
-app.listen(8080);
+const PORT = 8080;
+
+app.use('/app', express.static('./src/app'));
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
+
+function handlerReadiness(_: Request, res: Response) {
+  res.status(200).set('Content-Type', 'text/plain').send('OK');
+}
+app.get('/healthz', handlerReadiness);
