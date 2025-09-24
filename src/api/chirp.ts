f6616e0d5a8ce7f2e91082b4899a4766a822f988
Colin Eckert <colineckert10@gmail.com>
2025-09-23 18:58:30 -0700

    clean chirps on validate

--- src/api/chirp.ts
+++ src/api/chirp.ts
@@ -5,16 +5,38 @@ type RequestChirp = {
 };
 
 type ValidateChirpResponse = {
-  valid: boolean;
+  cleanedBody: string;
 };
 
 type ValidateChirpError = {
   error: string;
 };
 
+const badWords = ["kerfuffle", "sharbert", "fornax"];
+
+function containsBadWords(text: string): boolean {
+  const lowerText = text.toLowerCase();
+  return badWords.some((word) => lowerText.includes(word));
+}
+
+function cleanBadWords(text: string): string {
+  const cleanedText: string[] = [];
+
+  const words = text.split(" ");
+  for (const word of words) {
+    if (badWords.includes(word.toLowerCase())) {
+      cleanedText.push("****");
+    } else {
+      cleanedText.push(word);
+    }
+  }
+
+  return cleanedText.join(" ");
+}
+
 export function handlerValidateChirp(req: Request, res: Response) {
-  const resBody: ValidateChirpResponse = { valid: true };
   const chirp: RequestChirp = req.body;
+  const resBody: ValidateChirpResponse = { cleanedBody: chirp.body };
 
   if (chirp.body.length === 0) {
     const errBody: ValidateChirpError = { error: "Chirp cannot be empty" };
@@ -26,6 +48,10 @@ export function handlerValidateChirp(req: Request, res: Response) {
     const errBody: ValidateChirpError = { error: "Chirp is too long" };
     res.status(400).json(errBody);
     return;
+  }
+
+  if (containsBadWords(chirp.body)) {
+    resBody.cleanedBody = cleanBadWords(chirp.body);
   }
 
   res.status(200).json(resBody);
