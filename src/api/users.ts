5608e5c5c1bc04a15bf233fa2157c7638503da27
Colin Eckert <colineckert10@gmail.com>
2025-09-28 06:53:21 -0700

    save hashed password when creating users

--- src/api/users.ts
+++ src/api/users.ts
@@ -1,19 +1,26 @@
 import { Request, Response } from "express";
 import { createUser } from "../db/queries/users.js";
 import { BadRequestError } from "./errors.js";
+import { hashPassword } from "../auth.js";
 
 type CreateUserParams = {
   email: string;
+  password: string;
 };
 
 export async function handlerCreateUser(req: Request, res: Response) {
   const params: CreateUserParams = req.body;
 
-  if (!params.email) {
+  if (!params.email || !params.password) {
     throw new BadRequestError("Missing required create user fields");
   }
 
-  const user = await createUser(params);
+  const hashedPassword = await hashPassword(params.password);
+
+  const user = await createUser({
+    email: params.email,
+    hashed_password: hashedPassword,
+  });
 
   if (!user) {
     throw new Error("Failed to create user");
--- src/db/queries/users.ts
+++ src/db/queries/users.ts
@@ -1,13 +1,21 @@
 import { db } from "../index.js";
 import { NewUser, users } from "../schema.js";
 
-export async function createUser(user: NewUser) {
+type UserResponse = Omit<NewUser, "hashed_password">;
+
+export async function createUser(user: NewUser): Promise<UserResponse> {
   const [result] = await db
     .insert(users)
     .values(user)
     .onConflictDoNothing()
     .returning();
-  return result;
+
+  return {
+    id: result.id,
+    email: result.email,
+    createdAt: result.createdAt,
+    updatedAt: result.updatedAt,
+  };
 }
 
 export async function deleteAllUsers() {
